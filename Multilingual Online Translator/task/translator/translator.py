041fa5acf474be106ec583ce05cde1a53cbcca7d
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-27 00:30:03 +0100

    Implement basic menu

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,18 @@
-print('Hello, World!')
+"""Multilingual Online Translator Module"""
+
+
+def main():
+    display_menu()
+
+
+def display_menu():
+    print(
+        "Type \"en\" if you want to translate from French into English, "
+        "or \"fr\" if you want to translate from English into French:")
+    language = input()
+    word = input()
+    print(f"You chose \"{language}\" as a language to translate \'{word}\' to.")
+
+
+if __name__ == "__main__":
+    main()
